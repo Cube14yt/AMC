70eaee6bb615e90e6f3cea0af983f4ec98c1b85d
Cube14yt <veranoranzdave@gmail.com>
2026-04-08 18:41:53 +0800

    Add noble gasses

--- AAOMC.js
+++ AAOMC.js
@@ -19,61 +19,61 @@ console.log("vro just play the mod")
  * @returns The reactions generated
  */
 function make_deadly(name, type, speed, args = {}) {
-    const bug_entries = eLists.ANIMAL
-        .map(x => [x, { elem1: null, elem2: elements[x].breakInto, chance: speed }])
+	const bug_entries = eLists.ANIMAL
+		.map(x => [x, { elem1: null, elem2: elements[x].breakInto, chance: speed }])
 
-    elements.human.reactions[name] = { attr1: { panic: args.panic ?? 5 } }
+	elements.human.reactions[name] = { attr1: { panic: args.panic ?? 5 } }
 
-    switch (type) {
-        case "ACID":
-            elements[name].reactions =  {
-                "plant": { elem1: null, elem2: "dead_plant", chance: speed },
-                "head": { elem1: "bone", chance: speed},
-                "body": { elem1: "bone", chance: speed},
-                ...Object.fromEntries(bug_entries)
-            }
+	switch (type) {
+		case "ACID":
+			elements[name].reactions = {
+				"plant": { elem1: null, elem2: "dead_plant", chance: speed },
+				"head": { elem1: "bone", chance: speed },
+				"body": { elem1: "bone", chance: speed },
+				...Object.fromEntries(bug_entries)
+			}
 
-        case "POISON":
-            elements[name].reactions = {
-                "plant": { elem1: null, elem2: "dead_plant", chance: speed },
-                "head": { elem1: "rotten_meat", chance: speed},
-                "body": { elem1: "rotten_meat", chance: speed},
-                ...Object.fromEntries(bug_entries)
-            }
-    }
+		case "POISON":
+			elements[name].reactions = {
+				"plant": { elem1: null, elem2: "dead_plant", chance: speed },
+				"head": { elem1: "rotten_meat", chance: speed },
+				"body": { elem1: "rotten_meat", chance: speed },
+				...Object.fromEntries(bug_entries)
+			}
+	}
 }
 
 elements.bromine = {
 	density: 3102.8,
-	color: ["#401505","#401005","#401107","#3d190a"],
+	color: ["#401505", "#401005", "#401107", "#3d190a"],
 	tempLow: -7.2,
 	tempHigh: 58.8,
 	stateLow: "frozen_bromine",
 	stateHigh: "bromine_gas",
 	state: "liquid",
 	category: "liquids",
-    behavior: behaviors.LIQUID,
+	behavior: behaviors.LIQUID,
 	viscosity: 0.944,
 	reactions: {
-		"water": {elem1:null, elem2:"hydrobromic_acid"},
-		"propane": {elem1:"hydrobromic_acid", elem2:"brominated_hydrocarbons"},
-		"oil": {elem1:"hydrobromic_acid", elem2:"brominated_hydrocarbons"},
-		"lamp_oil": {elem1:"hydrobromic_acid", elem2:"brominated_hydrocarbons"},
-		"hydrogen": {elem1:"hydrobromic_acid", elem2:"hydrobromic_acid", tempMin:400},
-		"gold": {elem1:null, elem2:"gold_bromide", tempMin:140, tempMax:160},
-		"gold_coin": {elem1:null, elem2:"gold_bromide", tempMin:140, tempMax:160},
-		"iron": {elem1:null, elem2:"iron_bromide"},
-		"oxidized_copper": {elem1:"oxygen", elem2:"copper_bromide"},
-		"copper": {elem1:null, elem2:"copper_bromide"},
-		"silver": {elem1:null, elem2:"silver_bromide"},
-		"aluminum": {elem1:"smoke", elem2:"aluminum_bromide"},
-		"sodium": {elem1:"pop", elem2:"sodium_bromide"},
-		"copper": {elem1:null, elem2:"copper_bromide"},
-		"magnesium": {elem1:null, elem2:"magnesium_bromide"},
-		"sulfur": {elem1:[null,"disulfur_dibromide"], elem2:"disulfur_dibromide"},
-		"potassium": {elem1:"pop", elem2:"potassium_bromide"},
-		"calcium": {elem1:null, elem2:"calcium_bromide"},
-		"uranium": {elem1:"null", elem2:"uranium_pentabromide"}
+		"water": { elem1: null, elem2: "hydrobromic_acid" },
+		"propane": { elem1: "hydrobromic_acid", elem2: "brominated_hydrocarbons" },
+		"oil": { elem1: "hydrobromic_acid", elem2: "brominated_hydrocarbons" },
+		"lamp_oil": { elem1: "hydrobromic_acid", elem2: "brominated_hydrocarbons" },
+		"hydrogen": { elem1: "hydrobromic_acid", elem2: "hydrobromic_acid", tempMin: 400 },
+		"gold": { elem1: null, elem2: "gold_bromide", tempMin: 140, tempMax: 160 },
+		"gold_coin": { elem1: null, elem2: "gold_bromide", tempMin: 140, tempMax: 160 },
+		"iron": { elem1: null, elem2: "iron_bromide" },
+		"oxidized_copper": { elem1: "oxygen", elem2: "copper_bromide" },
+		"copper": { elem1: null, elem2: "copper_bromide" },
+		"silver": { elem1: null, elem2: "silver_bromide" },
+		"aluminum": { elem1: "smoke", elem2: "aluminum_bromide" },
+		"sodium": { elem1: "pop", elem2: "sodium_bromide" },
+		"copper": { elem1: null, elem2: "copper_bromide" },
+		"magnesium": { elem1: null, elem2: "magnesium_bromide" },
+		"sulfur": { elem1: [null, "disulfur_dibromide"], elem2: "disulfur_dibromide" },
+		"potassium": { elem1: "pop", elem2: "potassium_bromide" },
+		"calcium": { elem1: null, elem2: "calcium_bromide" },
+		"uranium": { elem1: "null", elem2: "uranium_pentabromide" }
 	}
 };
 make_deadly("bromine", "POISON", 0.5)
@@ -90,27 +90,27 @@ elements.ytterbium = {
 	conduct: 0.6,
 	hardness: 0.2,
 	reactions: {
-	  "water": {elem1:"ytterbium_hydroxide", elem2:"hydrogen"},
-	  "hydrogen": {elem1:"ytterbium_hydride", elem2:null},
-	  "chlorine": {elem1:"ytterbium_chloride", elem2:null},
-	  "bromine": {elem1:"ytterbium_bromide", elem2:null},
-	  "hydrobromic_acid": {elem1:"ytterbium_bromide", elem2:"hydrogen"},
-	  "acid": {elem1:"ytterbium_chloride", elem2:"hydrogen"},
-	  "neutral_acid": {elem1:"ytterbium_chloride", elem2:["hydrogen","water"]},
+		"water": { elem1: "ytterbium_hydroxide", elem2: "hydrogen" },
+		"hydrogen": { elem1: "ytterbium_hydride", elem2: null },
+		"chlorine": { elem1: "ytterbium_chloride", elem2: null },
+		"bromine": { elem1: "ytterbium_bromide", elem2: null },
+		"hydrobromic_acid": { elem1: "ytterbium_bromide", elem2: "hydrogen" },
+		"acid": { elem1: "ytterbium_chloride", elem2: "hydrogen" },
+		"neutral_acid": { elem1: "ytterbium_chloride", elem2: ["hydrogen", "water"] },
 	},
-tick: function(pixel){
-  for (let i = 0; i<adjacentCoords.length; i++){
-    let coord = adjacentCoords[i]
-    let x = pixel.x + coord[0]
-    let y = pixel.y + coord[1]
-    if (isEmpty(x, y)){
-      if (Math.random()<0.0001){
-        changePixel(pixel, "ytterbium_oxide")
-        return
-      }
-    }
-  }
-}
+	tick: function (pixel) {
+		for (let i = 0; i < adjacentCoords.length; i++) {
+			let coord = adjacentCoords[i]
+			let x = pixel.x + coord[0]
+			let y = pixel.y + coord[1]
+			if (isEmpty(x, y)) {
+				if (Math.random() < 0.0001) {
+					changePixel(pixel, "ytterbium_oxide")
+					return
+				}
+			}
+		}
+	}
 };
 elements.molten_ytterbium = {
 	density: 6500,
@@ -133,12 +133,12 @@ elements.ytterbium_hydroxide = {
 	state: "solid",
 	category: "solids",
 	behavior: behaviors.WALL,
-    conduct: 0.6,
+	conduct: 0.6,
 	hardness: 0.2,
 	reactions: {
-	  "hydrobromic_acid": {elem1:"ytterbium_bromide", elem2:"hydrogen"},
-	  "acid": {elem1:"ytterbium_chloride", elem2:"hydrogen"},
-	  "neutral_acid": {elem1:"ytterbium_chloride", elem2:["hydrogen","water"]},
+		"hydrobromic_acid": { elem1: "ytterbium_bromide", elem2: "hydrogen" },
+		"acid": { elem1: "ytterbium_chloride", elem2: "hydrogen" },
+		"neutral_acid": { elem1: "ytterbium_chloride", elem2: ["hydrogen", "water"] },
 	},
 };
 //Ytterbium Oxide (Yb₂O₃)
@@ -151,9 +151,9 @@ elements.ytterbium_oxide = {
 	category: "powders",
 	behavior: behaviors.POWDER,
 	reactions: {
-	  "hydrobromic_acid": {elem1:"ytterbium_bromide", elem2:"hydrogen"},
-	  "acid": {elem1:"ytterbium_chloride", elem2:"hydrogen"},
-	  "neutral_acid": {elem1:"ytterbium_chloride", elem2:["hydrogen","water"]},
+		"hydrobromic_acid": { elem1: "ytterbium_bromide", elem2: "hydrogen" },
+		"acid": { elem1: "ytterbium_chloride", elem2: "hydrogen" },
+		"neutral_acid": { elem1: "ytterbium_chloride", elem2: ["hydrogen", "water"] },
 	},
 };
 elements.molten_ytterbium_oxide = {
@@ -177,7 +177,7 @@ elements.ytterbium_hydride = {
 	category: "powders",
 	behavior: behaviors.POWDER,
 	reactions: {
-	  "water": {elem1:"ytterbium_hydroxide", elem2:null},
+		"water": { elem1: "ytterbium_hydroxide", elem2: null },
 	},
 };
 //Ytterbium Chloride (YbCl₃)
@@ -190,7 +190,7 @@ elements.ytterbium_chloride = {
 	category: "powders",
 	behavior: behaviors.POWDER,
 	reactions: {
-	  "water": {elem1:"ytterbium_hydroxide", elem2:null},
+		"water": { elem1: "ytterbium_hydroxide", elem2: null },
 	}
 };
 elements.molten_ytterbium_chloride = {
@@ -218,7 +218,7 @@ elements.molten_ytterbium_bromide = {
 	density: 5070,
 	color: ["#fbceb1", "#ffaa66"],
 	temp: 500,
-	tempLow: 300, 
+	tempLow: 300,
 	stateLow: "ytterbium_bromide",
 	state: "liquid",
 	category: "states",
@@ -226,13 +226,159 @@ elements.molten_ytterbium_bromide = {
 	fireColor: ["#39ff14", "#00913f"],
 };
 //Ytterbium stuff by Ytterbium
-elements.acid.name="hydrochloric_acid"
+elements.acid.name = "hydrochloric_acid"
 if (langCode === "xem") {
-    lang.bromine = "🟧☠️💧";
-},
+	lang.bromine = "🟧☠️💧";
+}
 if (langCode === "qha") {
-    lang.bromine = "samocomomae-e-alama";
-},
+	lang.bromine = "samocomomae-e-alama";
+}
 if (langCode === "qha") {
-    lang.ytterbium = "paemaelama-e-a-i-ehehalacihalama-elama";
-},
+	lang.ytterbium = "paemaelama-e-a-i-ehehalacihalama-elama";
+}
+
+// Noble gases by Cube14 (Hydrogen)
+elements.argon = {
+	color: "#b5a1bd",
+	behavior: behaviors.GAS,
+	tempLow: -185.848,
+	category: "gases",
+	density: 1.784,
+	state: "gas",
+	conduct: 0.5,
+	colorOn: ["#e100ff", "#bd00d6", "#a101b6"]
+}
+
+elements.liquid_argon = {
+	tempLow: -189.34,
+	colorOn: ["#e100ff", "#bd00d6", "#a101b6"]
+}
+
+elements.argon_ice = {
+	colorOn: ["#e100ff", "#bd00d6", "#a101b6"]
+}
+
+elements.krypton = {
+	color: "#94829b",
+	behavior: behaviors.GAS,
+	tempLow: -153.415,
+	category: "gases",
+	density: 3.749,
+	state: "gas",
+	conduct: 0.4,
+	colorOn: ["#cdadd1", "#a789aa"]
+}
+
+elements.liquid_krypton = {
+	tempLow: -157.37,
+	colorOn: ["#cdadd1", "#a789aa"]
+}
+
+elements.krypton_ice = {
+	colorOn: ["#cdadd1", "#a789aa"]
+}
+
+elements.xenon = {
+	color: "#bbbce7",
+	behavior: behaviors.GAS,
+	tempLow: -108.099,
+	category: "gases",
+	density: 5.894,
+	state: "gas",
+	conduct: 0.4,
+	colorOn: ["#74a3cf", "#3790cc", "#2879c5"]
+}
+
+elements.liquid_xenon = {
+	tempLow: -111.75,
+	colorOn: ["#74a3cf", "#3790cc", "#2879c5"]
+}
+
+elements.xenon_ice = {
+	colorOn: ["#74a3cf", "#3790cc", "#2879c5"]
+}
+
+if (!elements?.neutron?.reactions) elements.neutron.reactions = {}
+
+elements.neutron.reactions.radon = { temp2:100 }
+elements.neutron.reactions.liquid_radon = { temp2:100 }
+elements.neutron.reactions.radon_ice = { temp2:100 }
+
+elements.radon = {
+	color: "#abc0a4",
+	behavior: behaviors.GAS,
+	tempLow: -61.7,
+	category: "gases",
+	density: 9.73,
+	state: "gas",
+	conduct: 0.1,
+	colorOn: ["#c2eec2", "#bdeeb1"],
+	tick(pixel) {
+		if (Math.random() <= 0.01) {
+			releaseElement(pixel, "radiation")
+		}
+		if (Math.random() <= 0.0005) {
+			const alpha_particle = releaseElement(pixel, "helium")
+			if (alpha_particle) {
+				alpha_particle.temp += 300
+			}
+			changePixel(pixel, "polonium")
+			pixel.temp += 600
+		}
+	},
+	reactions: {
+		"neutron": {elem1: "n_explosion", tempMin: 400, chance: 0.1}
+	}
+}
+
+elements.liquid_radon = {
+	tempLow: -71,
+	colorOn: ["#c2eec2", "#bdeeb1"],
+	tick: elements.radon.tick,
+}
+
+elements.radon_ice = {
+	colorOn: ["#c2eec2", "#bdeeb1"],
+	tick: elements.radon.tick,
+}
+
+elements.oganesson = {
+	color: elements.neon.color,
+	behavior: behaviors.GAS,
+	colorOn: ["#b5e65b", "#b9f756", "#aee456"],
+	category: "gases",
+	state: "gas",
+	conduct: 0.2,
+	density: 9.73,
+	tick(pixel) {
+		releaseElement(pixel, "radiation", 5)
+		if (Math.random() <= 0.5) {
+			changePixel(pixel, "transuranium_explosion")
+			return
+		}
+		if (Math.random() <= 0.3) {
+			const alpha_particle = releaseElement(pixel, "helium")
+			if (alpha_particle) {
+				alpha_particle.temp += 1000
+			}
+			changePixel(pixel, "livermormium")
+			pixel.temp += 1000
+		}
+	}
+}
+
+elements.transuranium_explosion = {
+	color: ["#a7ff5f", "#54fa83", "#53fdc5"],
+	behavior: [
+		"XX|XX|XX",
+		"XX|EX:80>plasma,plasma,plasma,plasma,radiation,rad_steam|XX",
+		"XX|XX|XX"
+	],
+	temp: 10000000000,
+	category: "energy",
+	state: "gas",
+	density: 1000,
+	excludeRandom: true,
+	hidden: true,
+	noMix: true
+}
